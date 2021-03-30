e31b5aa36411c074c3e3db5a73d3ad9eb100a852
Adam Dede <30808263+adede1988@users.noreply.github.com>
2021-03-30 13:41:41 +0700

    datUpdate

--- AB_pic_singleT2_lastrun.py
+++ AB_pic_singleT2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on March 24, 2021, at 10:48
+    on March 29, 2021, at 09:31
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
